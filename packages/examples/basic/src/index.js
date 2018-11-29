4d7037f0f89270c299e3913467887bf3003996df
Harry Solovay <harrysolovay@gmail.com>
2018-11-29 10:02:20 -0500

    use-stylelint-config for css, scss & jss

--- packages/examples/basic/src/index.js
+++ packages/examples/basic/src/index.js
@@ -1,9 +1,14 @@
 import styled, {createGlobalStyle} from 'styled-components'
+import normalize from 'styled-normalize'
 import '~/styles.css'
 import {render} from 'react-dom'
 import {registerServiceWorker} from '~/utilities'
 
 const GlobalStyles = createGlobalStyle`
+  ${normalize}
+  * {
+    box-sizing: border-box;
+  }
   html, body {
     width: 100%;
     height: 100%;
@@ -19,13 +24,14 @@ const Container = styled.div`
   align-items: center;
   text-align: center;
   background-color: #fdee59;
+  border: 10px solid #5186ec;
 `
 
 const App = () => (
   <>
     <GlobalStyles />
     <Container>
-      <header children='Rescripted' />
+      <header children='Rescripts' />
       <a
         children={`check out the docs`}
         href='https://github.com/rescripts/rescripts'
--- packages/rescripts/use-stylelint-config/index.js
+++ packages/rescripts/use-stylelint-config/index.js
@@ -1,35 +1,31 @@
 const {
+  propEq,
   findIndex,
-  map,
   lensPath,
   over,
-  propEq,
+  map,
   append,
-  includes,
   compose,
 } = require('ramda')
 const styleLint = require('styleLint')
-const {resolveFromRootOrNodeModules, error} = require('@rescripts/utilities')
+const {resolveFromRootOrNodeModules} = require('@rescripts/utilities')
 
 const postCSSLoaderPath = require.resolve('postcss-loader')
 const isPostCSSLoader = propEq('loader', postCSSLoaderPath)
 const getPostCSSPluginsLens = ({use}) => {
-  if (!use) {
-    return false
-  }
   const postCSSLoaderIndex = findIndex(isPostCSSLoader, use)
   return postCSSLoaderIndex >= 0
     ? lensPath(['use', postCSSLoaderIndex, 'options', 'plugins'])
     : false
 }
 
-const addPostCSSStyleLintPlugin = path =>
+const addStyleLintPluginToPostCSSLoaders = path =>
   over(
     lensPath(['module', 'rules', 2, 'oneOf']),
     map(oneOf => {
       try {
-        const lens = getPostCSSPluginsLens({configFile: path})
-        return over(lens, fn => [...fn(), styleLint], oneOf)
+        const lens = getPostCSSPluginsLens(oneOf)
+        return over(lens, fn => [...fn(), styleLint({configFile: path})], oneOf)
       } catch (e) {
         return oneOf
       }
@@ -37,29 +33,27 @@ const addPostCSSStyleLintPlugin = path =>
   )
 
 // bug in 'stylelint-custom-processor-loader' configPath resolution
-const addStylelintCustomProcessorLoader = over(
-  lensPath(['module', 'rules', 1, 'use']),
-  append({
-    loader: 'stylelint-custom-processor-loader',
-    options: {
-      configPath: null,
-      emitWarning: true,
-    },
-  }),
-)
+const addStylelintCustomProcessorLoader = path =>
+  over(
+    lensPath(['module', 'rules', 1, 'use']),
+    append({
+      loader: require.resolve('stylelint-custom-processor-loader'),
+      options: {
+        configPath: null,
+        emitWarning: true,
+      },
+    }),
+  )
+
+const formatTransformMap = {
+  css: addStyleLintPluginToPostCSSLoaders,
+  scss: addStyleLintPluginToPostCSSLoaders,
+  js: addStylelintCustomProcessorLoader,
+}
 
 module.exports = ({path, formats}) => config => {
   const resolved = resolveFromRootOrNodeModules(path)
-  const transforms = map(
-    e => {
-      const [format, transform] = e
-      return includes(format, formats) && transform
-    },
-    [
-      ['css', addPostCSSStyleLintPlugin(resolved)],
-      ['js', addStylelintCustomProcessorLoader],
-    ],
-  ).filter(Boolean)
+  const transforms = map(key => formatTransformMap[key](resolved), formats)
   const transform = compose(...transforms)
   return transform(config)
 }
