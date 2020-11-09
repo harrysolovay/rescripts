e725488330e1da72170712eeb6dff4f30cdb3994
Coteh <3276350+Coteh@users.noreply.github.com>
2020-11-08 21:19:38 -0500

    #109 Add error reporter to path loaders

--- packages/utilities/errors.js
+++ packages/utilities/errors.js
@@ -1,8 +1,13 @@
 const error = message => {
   console.error(message)
+}
+
+const exitWithError = message => {
+  error(message)
   process.exit(1)
 }
 
 module.exports = {
   error,
+  exitWithError,
 }
